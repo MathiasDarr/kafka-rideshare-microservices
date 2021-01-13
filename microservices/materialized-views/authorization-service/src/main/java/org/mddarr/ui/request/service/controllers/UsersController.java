0e3bd660724187f84e03a30ad337da6133bd848c
mddarr <mddarr@gmail.com>
2021-01-13 12:56:53 -0800

    write a test for the authorization route..

--- microservices/materialized-views/authorization-service/src/main/java/org/mddarr/ui/request/service/controllers/UsersController.java
+++ microservices/materialized-views/authorization-service/src/main/java/org/mddarr/ui/request/service/controllers/UsersController.java
@@ -1,13 +1,20 @@
 package org.mddarr.ui.request.service.controllers;
 
+import lombok.Getter;
+import org.mddarr.ui.request.service.MyUserDetailsService;
+import org.mddarr.ui.request.service.models.AuthenticationRequest;
+import org.mddarr.ui.request.service.models.AuthenticationResponse;
 import org.mddarr.ui.request.service.models.UserEntity;
 import org.mddarr.ui.request.service.models.requests.PostUserRequest;
 import org.mddarr.ui.request.service.services.UserService;
+import org.mddarr.ui.request.service.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,14 +24,54 @@ public class UsersController {
     @Autowired
     UserService userService;
 
-    @GetMapping(value="/users/{userid}")
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @GetMapping("/api/hello" )
+    public String firstPage() {
+        return "Hello World";
+    }
+
+
+    @GetMapping(value="/api/users/{userid}")
     public List<UserEntity> getUserDetail(String userid){
         return userService.getUserById(userid);
     }
 
-    @PutMapping(value = "/users")
+
+
+    @PutMapping(value = "/api/users")
     public String postUser(@RequestBody  PostUserRequest postUserRequest){
         return userService.postUser(postUserRequest);
     }
+
+    @PostMapping(value = "/api/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        }
+        catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+
+
+
 
 }
--- microservices/materialized-views/authorization-service/src/test/java/org/mddarr/ui/request/service/authorization/AuthorizationTests.java
+++ microservices/materialized-views/authorization-service/src/test/java/org/mddarr/ui/request/service/authorization/AuthorizationTests.java
@@ -1,13 +1,11 @@
-package org.mddarr.ui.request.service.api;
-
+package org.mddarr.ui.request.service.authorization;
 
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mddarr.ui.request.service.ContainersTestingServiceApplication;
-
-
+import org.mddarr.ui.request.service.MyUserDetailsService;
 import org.mddarr.ui.request.service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,18 +15,15 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = ContainersTestingServiceApplication.class)
-public class UsersApiTest {
+public class AuthorizationTests {
 
     private MockMvc mockMvc;
 
@@ -36,6 +31,8 @@ public class UsersApiTest {
     private WebApplicationContext webApplicationContext;
 
 
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
 
     @MockBean
     UserService userService;
@@ -48,33 +45,11 @@ public class UsersApiTest {
     @Test
     public void should_post_user() throws Exception {
 
-        mockMvc.perform(put("/api/users")
+        mockMvc.perform(post("/api/authenticate")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{ \"email\": \"SAVINGS\", \"first_name\": \"Jordan\", \"last_name\": \"Pence\"  }")
+                .content("{ \"username\": \"foo\", \"password\": \"foo\"}")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
-
-
     }
-
-
-//    @MockBean
-//    private AccountService accountServiceMock;
-
-//    public void should_create_account() throws Exception{     
-//
-//        when(accountServiceMock.createAccount(any(Account.class))).thenReturn(12345L);
-//
-//        mockMvc.perform(post("/api/account")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content("{ \"accountType\": \"SAVINGS\", \"balance\": 5000.0 }")
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isCreated())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(header().string("Location", "/api/account/12345"))
-//                .andExpect(jsonPath("$.accountId").value("12345"))
-//                .andExpect(jsonPath("$.accountType").value("SAVINGS"))
-//                .andExpect(jsonPath("$.balance").value(5000));
-//    }
 
 }
