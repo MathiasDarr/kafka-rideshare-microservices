0e9e5c094d6394d506e3e96484d47b39ab4f60d8
mddarr <mddarr@gmail.com>
2021-01-13 13:51:38 -0800

    Parse the jwt token in the repsonse to the authentication endpoint in the AuthorizationTests

--- microservices/materialized-views/authorization-service/src/test/java/org/mddarr/ui/request/service/authorization/AuthorizationTests.java
+++ microservices/materialized-views/authorization-service/src/test/java/org/mddarr/ui/request/service/authorization/AuthorizationTests.java
@@ -1,6 +1,8 @@
 package org.mddarr.ui.request.service.authorization;
 
 
+import com.google.gson.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,8 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 
@@ -46,11 +50,36 @@ public class AuthorizationTests {
     @Test
     public void should_authenticate() throws Exception {
 
-        mockMvc.perform(post("/api/authenticate")
+        MvcResult result = mockMvc.perform(post("/api/authenticate")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{ \"username\": \"foo\", \"password\": \"foo\"}")
+                .content("{ \"username\": \"cdick@gmail.com\", \"password\": \"password\"}")
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andReturn();
+        MockHttpServletResponse mockHttpServletResponse =  result.getResponse();
+
+        String jsonResponse = mockHttpServletResponse.getContentAsString();
+
+        JsonObject jobj = new Gson().fromJson(jsonResponse, JsonObject.class);
+        String token = jobj.get("jwt").toString();
+        System.out.println(token);
+
+//        JsonElement jelement = new JsonParser().parse(jsonResponse);
+//
+//        System.out.println(jelement);
+//
+//        System.out.println(jelement.get("jwt"));
+//        JsonObject jobject = jelement.getAsJsonObject();
+//        jobject = jobject.getAsJsonObject("jwt");
+//        JsonArray jarray = jobject.getAsJsonArray("translations");
+//        jobject = jarray.get(0).getAsJsonObject();
+//        String parsed_json = jobject.get("translatedText").getAsString();
+
+
+//        System.out.println(mockHttpServletResponse.getContentAsString());
+//
+//        System.out.println("THE PARSED JSON IS " + jobject.getAsString());
+
     }
 
 }
